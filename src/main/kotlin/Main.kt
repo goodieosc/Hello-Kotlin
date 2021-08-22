48717362703eaf9231a4a7dfd80d7b676a10ac2c
goodie-org <75117599+goodie-org@users.noreply.github.com>
2021-08-22 18:07:01 +0800

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+import java.time.DayOfWeek
+import java.util.*
+
+fun main(args: Array<String>) {
+
+
+    //val hour = Calendar.HOUR_OF_DAY
+    //val minute = Calendar.MINUTE
+
+    //println("${hour.toString()} ${minute.toString()}")
+
+
+    if (Calendar.HOUR_OF_DAY < 12) println("Good morning, Kotlin") else  println("Good night, Kotlin")
+
+
+
+}
+
